7f5fef3f4313a790e7389044952c8c6e50906a71
hafizmfadli <lshafizmf1st@gmail.com>
2021-05-03 13:25:15 +0700

    Update file migrasi

--- resources/migrations/20210430171340-create-table.cjs
+++ resources/migrations/20210430171340-create-table.cjs
@@ -8,42 +8,6 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    // await queryInterface.createTable('Orders', {
-    //   id: {
-    //     type: Sequelize.INTEGER,
-    //     primaryKey: true,
-    //     allowNull: false,
-    //     autoIncrement: true
-    //   },
-    //   status: Sequelize.STRING,
-    //   invoiceNumber: Sequelize.STRING,
-    //   createdAt: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE
-    //   },
-    //   updatedAt: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE
-    //   }
-    // })
-
-    // await queryInterface.createTable('Customers', {
-    //   id: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     primaryKey: true,
-    //     autoIncrement: true
-    //   },
-    //   name: Sequelize.STRING,
-    //   createdAt: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE
-    //   },
-    //   updatedAt: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE
-    //   }
-    // })
     
     await queryInterface.createTable('mahasiswa', {
       id_mahasiswa: {
@@ -58,6 +22,26 @@ module.exports = {
       nomor_hp: Sequelize.STRING,
       url_foto: Sequelize.STRING,
       status: Sequelize.STRING,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }  
+    })
+
+    await queryInterface.createTable('keterangan', {
+      id_keterangan: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      // id_mahasiswa (fk)
+      status: Sequelize.STRING,
+      url: Sequelize.STRING,
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -122,6 +106,199 @@ module.exports = {
       }
     })
 
+    await queryInterface.createTable('mata_kuliah', {
+      id_mata_kuliah: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: false
+      },
+      // kode_program_studi (fk) 
+      semester: Sequelize.INTEGER,
+      nama_mata_kuliah: Sequelize.STRING,
+      sks_teori: Sequelize.INTEGER,
+      sks_praktek: Sequelize.INTEGER,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+
+    await queryInterface.createTable('dosen', {
+      NIP: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: false
+      },
+      nama_dosen: Sequelize.STRING,
+      jabatan: Sequelize.STRING,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+  
+    })
+
+    await queryInterface.createTable('tata_usaha', {
+      id_tata_usaha: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+
+    await queryInterface.createTable('jadwal', {
+      id_jadwal: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+      },
+      // id_dosen (fk)
+      ja: {
+        type: Sequelize.INTEGER
+      },
+      jb: {
+        type: Sequelize.INTEGER
+      },
+      waktu_mulai: {
+        type: Sequelize.TIME
+      },
+      waktu_selesai: {
+        type: Sequelize.TIME
+      },
+      batas_terakhir_absen: {
+        type: Sequelize.TIME
+      },
+      hari: {
+        type: Sequelize.INTEGER
+      },
+      jenis: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }  
+    })
+
+    await queryInterface.createTable('perkuliahan', {
+      id_perkuliahan: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      // id_mata_kuliah (fk)
+      // kode_kelas (fk)
+      tahun_akademik: Sequelize.STRING,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+
+    await queryInterface.createTable('studi', {
+      id_studi: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+      // id_mahasiswa (fk)
+      // id_mata_kuliah (fk)
+      // kode_kelas (fk)
+    })
+    
+    await queryInterface.createTable('daftar_hadir_mahasiswa', {
+      id_daftar_hadir_mhs: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true
+      },
+      // id_studi (fk)
+      // id_keterangan (fk)
+      keterlambatan: {
+        type: Sequelize.INTEGER
+      },
+      tanggal: {
+        type: Sequelize.DATE
+      },
+      isHadir: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+
+    await queryInterface.createTable('daftar_hadir_dosen', {
+      id_daftar_hadir_dosen: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true
+      },
+      // id_studi (fk)
+      // NIP (fk)
+      tanggal: {
+        type: Sequelize.DATE
+      },
+      isHadir: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+
+
+    // Untuk tabel pengajar dibuat diakhir, karena semua attributenya hasil turunan tabel lain
 
   },
 
@@ -132,5 +309,32 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+
+    // drop yang ngereferences ke table lain dahulu
+    await queryInterface.dropTable('daftar_hadir_dosen')
+    await queryInterface.dropTable('daftar_hadir_mahasiswa')
+    await queryInterface.dropTable('studi')
+    await queryInterface.dropTable('jadwal')
+    await queryInterface.dropTable('perkuliahan')
+    await queryInterface.dropTable('mata_kuliah')
+    await queryInterface.dropTable('keterangan')
+    await queryInterface.dropTable('mahasiswa')
+    await queryInterface.dropTable('kelas')
+    await queryInterface.dropTable('program_studi')
+    await queryInterface.dropTable('jurusan')
+    
+    await queryInterface.dropTable('tata_usaha')
+    await queryInterface.dropTable('dosen')    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
   }
 };
--- resources/migrations/20210430172738-add-associations.cjs
+++ resources/migrations/20210430172738-add-associations.cjs
@@ -8,15 +8,7 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    // await queryInterface.addColumn('Orders', 'CustomerId', {
-    //   type: Sequelize.INTEGER,
-    //   references: {
-    //     model: 'Customers',
-    //     key: 'id'
-    //   },
-    //   onUpdate: 'CASCADE',
-    //   onDelete: 'SET NULL'
-    // })
+    
     
     await queryInterface.addColumn('mahasiswa', 'kode_kelas', {
       type: Sequelize.INTEGER,
@@ -36,13 +28,13 @@ module.exports = {
       }
     })
 
-    // await queryInterface.addColumn('keterangan', 'id_mahasiswa', {
-    //   type: Sequelize.INTEGER,
-    //   references: {
-    //     model: 'mahasiswa',
-    //     key: 'id_mahasiswa'
-    //   }
-    // })
+    await queryInterface.addColumn('keterangan', 'id_mahasiswa', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'mahasiswa',
+        key: 'id_mahasiswa'
+      }
+    })
 
     await queryInterface.addColumn('program_studi', 'kode_jurusan', {
       type: Sequelize.STRING,
@@ -52,34 +44,105 @@ module.exports = {
       }
     })
 
-    // await queryInterface.addColumn('mata_kuliah', 'kode_program_studi', {
-    //   type: Sequelize.STRING,
-    //   references: {
-    //     model: 'program_studi',
-    //     key: 'kode_program_studi'
-    //   }
-    // })
+    await queryInterface.addColumn('mata_kuliah', 'kode_program_studi', {
+      type: Sequelize.STRING,
+      references: {
+        model: 'program_studi',
+        key: 'kode_program_studi'
+      }
+    })
 
-    // await queryInterface.addColumn('perkuliahan', 'kode_kelas', {
+    await queryInterface.addColumn('perkuliahan', 'kode_kelas', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'kelas',
+        key: 'kode_kelas'        
+      }
+    })
+
+    await queryInterface.addColumn('perkuliahan', 'id_mata_kuliah', {
+      type: Sequelize.STRING,
+      references: {
+        model: 'mata_kuliah',
+        key: 'id_mata_kuliah'        
+      }
+    })
+
+
+    // Pendefinisian composite key
+    await queryInterface.sequelize.query('ALTER TABLE "perkuliahan" ADD CONSTRAINT "id_perkuliahan" UNIQUE ("id_mata_kuliah", "kode_kelas")')
+
+    await queryInterface.addColumn('studi', 'id_mahasiswa', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'mahasiswa',
+        key: 'id_mahasiswa'
+      }
+    })
+
+    await queryInterface.addColumn('studi', 'id_perkuliahan', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'perkuliahan',
+        key: 'id_perkuliahan'
+      }
+    })
+
+    // await queryInterface.addColumn('studi', 'kode_kelas', {
     //   type: Sequelize.INTEGER,
     //   references: {
-    //     model: 'kelas',
-    //     key: 'kode_kelas'        
+    //     model: 'perkuliahan',
+    //     key: 'kode_kelas'
     //   }
     // })
 
-    // await queryInterface.addColumn('perkuliahan', 'id_mata_kuliah', {
-    //   type: Sequelize.STRING,
-    //   references: {
-    //     model: 'mata_kuliah',
-    //     key: 'id_mata_kuliah'        
-    //   }
-    // })
+    await queryInterface.addColumn('daftar_hadir_dosen', 'id_studi', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'studi',
+        key: 'id_studi'
+      }
+    })
 
-    // // Pendefinisian composite key
-    // await queryInterface.sequelize.query('ALTER TABLE "perkuliahan" ADD CONSTRAINT "id_perkuliahan" PRIMARY KEY ("id_mata_kuliah", "kode_kelas")')
+    await queryInterface.addColumn('daftar_hadir_dosen', 'NIP', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'dosen',
+        key: 'NIP'
+      }
+    })
 
+    await queryInterface.addColumn('daftar_hadir_mahasiswa', 'id_studi', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'studi',
+        key: 'id_studi'
+      }
+    })
 
+    await queryInterface.addColumn('daftar_hadir_mahasiswa', 'id_keterangan', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'keterangan',
+        key: 'id_keterangan'
+      }
+    })
+
+    await queryInterface.addColumn('jadwal', 'NIP', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'dosen',
+        key: 'NIP'
+      }
+    })
+
+    await queryInterface.addColumn('jadwal', 'id_perkuliahan', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'perkuliahan',
+        key: 'id_perkuliahan'
+      }
+    })
     
 
   },
@@ -91,5 +154,12 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.removeColumn('mahasiswa', 'kode_kelas')
+    await queryInterface.removeColumn('kelas', 'kode_program_studi')
+    await queryInterface.removeColumn('keterangan', 'id_mahasiswa')
+    await queryInterface.removeColumn('program_studi', 'kode_jurusan')
+    await queryInterface.removeColumn('mata_kuliah', 'kode_program_studi')
+    await queryInterface.removeColumn('perkuliahan', 'kode_kelas')
+    await queryInterface.removeColumn('perkuliahan', 'id_mata_kuliah')
   }
 };
