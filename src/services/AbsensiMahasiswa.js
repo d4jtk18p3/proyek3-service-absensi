763026c02b1ae969400e1857e317d7fcd15e4187
hafizmfadli <lshafizmf1st@gmail.com>
2021-05-19 22:12:49 +0700

    Update service absensi mahasiswa
    Fix get nim dari data studi

--- src/services/AbsensiMahasiswa.js
+++ src/services/AbsensiMahasiswa.js
@@ -1,13 +1,24 @@
 import * as KeteranganDAO from '../dao/Keterangan'
 import * as DaftarHadirMahasiswaDAO from '../dao/DaftarHadirMahasiswa'
+import * as StudiDAO from '../dao/Studi'
 
 export const ajukanIzin = async (idStudies, status, url) => {
   // ide :
   // insert keterangan terlebih dahulu (karena kita butuh id nya)
   // id_keterangan dan id studi akan menjadi FK pada tabel daftar_hadir_mhs
 
+  // Input:
+  // idStudies : array of id_studi
+  // status : char (i = izin, s = sakit, a = alfa)
+  // urgl : string (url image surat izin)
+
+  // Output :
+  // data daftar hadir berhasil disimpan di db
+  // return daftar daftar hadir mahasiswa yang mengajukan izin
+
   try {
-    const nim = idStudies[0].id_mahasiswa
+    const nim = (await StudiDAO.findStudiByIdStudi(idStudies[0])).dataValues.id_mahasiswa
+    console.log("NIM : ", nim)
     const keterangan = await KeteranganDAO.insertKeterangan(nim, status, url)
     const date = new Date()
     const results = await Promise.all(
