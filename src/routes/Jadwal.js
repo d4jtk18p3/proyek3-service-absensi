d1a0f8b9bd200cc9f98d8596aac50895071e30f9
hafizmfadli <lshafizmf1st@gmail.com>
2021-05-20 19:18:28 +0700

    Update routes jadwal

--- src/routes/Jadwal.js
+++ src/routes/Jadwal.js
@@ -4,6 +4,8 @@ import * as JadwalController from '../controller/Jadwal'
 const router = express.Router()
 
 router.get('/dosen', JadwalController.getJadwalDosenByHariAndNIP)
-router.get('/mahasiswa', JadwalController.getJadwalMahasiswaByHariAndNIM)
+// router.get('/mahasiswa', JadwalController.getJadwalMahasiswaByHariAndNIM)
+router.get('/mahasiswa', JadwalController.getJadwalMhsHrTertentuHandler)
+
 
 export default router
