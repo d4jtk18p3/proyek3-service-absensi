521aa7b45c78a724b71f05d8752d096a282bc03a
hafizmfadli <lshafizmf1st@gmail.com>
2021-05-27 01:47:02 +0700

    Menambahkan controller DaftarHadirMahasiswa.js

---
+++ src/controller/DaftarHadirMahasiswa.js
@@ -0,0 +1,11 @@
+import * as MahasiswaService  from '../services/Mahasiswa'
+
+export const presensiMhsHandler = async(req, res, next) => {
+  const {idStudi, idJadwal} = req.query
+  try {
+    const result = await MahasiswaService.melakukanAbsensi(idStudi, idJadwal)
+    res.json(result)
+  } catch (error) {
+    next(error)
+  }
+}
