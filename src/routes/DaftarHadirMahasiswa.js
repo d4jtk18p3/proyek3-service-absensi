eab80e06a7254130d972a2c3b388f4d01bc80ebb
hafizmfadli <lshafizmf1st@gmail.com>
2021-06-02 22:35:33 +0700

    Menambahkan route

--- src/routes/DaftarHadirMahasiswa.js
+++ src/routes/DaftarHadirMahasiswa.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.put('/presensi', DaftarHadirMahasiswaController.presensiMhsHandler)
 router.get('/kelas-jadwal', DaftarHadirMahasiswaController.getDaftarHadirKelasJadwal)
+router.get('/nim-jadwal-tgl', DaftarHadirMahasiswaController.getDaftarHadirNimJadwalTgl)
 
 export default router
