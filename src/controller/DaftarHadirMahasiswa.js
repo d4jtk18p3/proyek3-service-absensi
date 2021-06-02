6e1111cf15f6f66423abddbc00596dc6b12164d3
hafizmfadli <lshafizmf1st@gmail.com>
2021-06-02 22:31:02 +0700

    Menambahkan controller baru
    Controller unutk get daftar hadir berdasarkan nim-jadwal-tgl

--- src/controller/DaftarHadirMahasiswa.js
+++ src/controller/DaftarHadirMahasiswa.js
@@ -2,6 +2,8 @@ import * as MahasiswaService from '../services/Mahasiswa'
 import * as DaftarHadirMahasiswaService from '../services/DaftarHadirMahasiswa'
 
 export const presensiMhsHandler = async (req, res, next) => {
+  // Author : hafizmfadli
+  // response : daftar hadir suatu mhs yang melakukan presensi
   const { idStudi, idJadwal } = req.query
   try {
     const result = await MahasiswaService.melakukanAbsensi(idStudi, idJadwal)
@@ -17,11 +19,27 @@ export const presensiMhsHandler = async (req, res, next) => {
 }
 
 export const getDaftarHadirKelasJadwal = async (req, res, next) => {
+  // Author : hafizmfadli
+  // response : daftar hadir seluruh mahasiswa pada suatu kelas pd tgl dan jadwal tertentu
   const { kodeKelas, idJadwal, tanggal } = req.query
   try {
     const result = await DaftarHadirMahasiswaService.getDaftarHadirKelasJadwal(kodeKelas, idJadwal, tanggal)
     res.json({
       message: `daftar hadir kelas ${kodeKelas} pada jadwal ${idJadwal} pada tanggal ${tanggal}`,
+      data: result
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getDaftarHadirNimJadwalTgl = async (req, res, next) => {
+  const { nim, idJadwal, tanggal } = req.query
+
+  try {
+    const result = await DaftarHadirMahasiswaService.getDaftarHadirNimJadwalTanggal(nim, idJadwal, tanggal)
+    res.json({
+      message: `daftar hadir ${nim} pada jadwal ${idJadwal} pada tanggal ${tanggal}`,
       data: result
     })
   } catch (error) {
