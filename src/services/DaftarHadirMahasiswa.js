001c38118ff14dd4e0a6d3547f9a09794b9bd168
hafizmfadli <lshafizmf1st@gmail.com>
2021-06-02 22:34:41 +0700

    Menambahkan service get daftar hadir by nim-jadwal-tgl

--- src/services/DaftarHadirMahasiswa.js
+++ src/services/DaftarHadirMahasiswa.js
@@ -1,11 +1,20 @@
 import * as DaftarHadirMahasiswaDAO from '../dao/DaftarHadirMahasiswa'
 
 export const getDaftarHadirKelasJadwal = async (kodeKelas, idJadwal, tanggal) => {
-  
+
   try {
    const result = await DaftarHadirMahasiswaDAO.getDaftarHadirKelasJadwal(kodeKelas, idJadwal, tanggal)
    return result 
   } catch (error) {
     return Promise.reject(error)
+  }
+}
+
+export const getDaftarHadirNimJadwalTanggal = async (nim, idJadwal, tanggal) => {
+  try {
+    const result = await DaftarHadirMahasiswaDAO.getByNimJadwalTgl(nim, idJadwal, tanggal)
+    return result
+  } catch (error) {
+    return Promise.reject(error)    
   }
 }
