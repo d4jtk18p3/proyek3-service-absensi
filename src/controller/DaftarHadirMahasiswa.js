cafaf6b1eec4820a7442b2f64fb3c7d1369c2e18
hafizmfadli <lshafizmf1st@gmail.com>
2021-06-02 19:19:56 +0700

    Menambahkan route handler untuk get daftar hadir kelas-jadwal

--- src/controller/DaftarHadirMahasiswa.js
+++ src/controller/DaftarHadirMahasiswa.js
@@ -1,4 +1,5 @@
 import * as MahasiswaService from '../services/Mahasiswa'
+import * as DaftarHadirMahasiswaService from '../services/DaftarHadirMahasiswa'
 
 export const presensiMhsHandler = async (req, res, next) => {
   const { idStudi, idJadwal } = req.query
@@ -9,6 +10,19 @@ export const presensiMhsHandler = async (req, res, next) => {
       data: {
         presensi: result
       }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getDaftarHadirKelasJadwal = async (req, res, next) => {
+  const { kodeKelas, idJadwal, tanggal } = req.query
+  try {
+    const result = await DaftarHadirMahasiswaService.getDaftarHadirKelasJadwal(kodeKelas, idJadwal, tanggal)
+    res.json({
+      message: `daftar hadir kelas ${kodeKelas} pada jadwal ${idJadwal} pada tanggal ${tanggal}`,
+      data: result
     })
   } catch (error) {
     next(error)
