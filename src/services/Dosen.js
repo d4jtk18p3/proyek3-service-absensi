9239f2b4940dd16ddc7fed8cf8b2a57e422ef98a
hafizmfadli <lshafizmf1st@gmail.com>
2021-06-03 06:35:42 +0700

    Menambahkan service update kehadiran mahasiswa

--- src/services/Dosen.js
+++ src/services/Dosen.js
@@ -2,6 +2,8 @@
   Catatan :  File ini berisi seluruh bussiness logic yang dapat dilakukan oleh mahasiswa
 */
 import * as DaftarHadirDosenDAO from '../dao/DaftarHadirDosen'
+import * as JadwalDAO from '../dao/Jadwal'
+import * as DaftarHadirMahasiswaDAO from '../dao/DaftarHadirMahasiswa'
 
 export const melakukanAbsensi = async (nip, idStudi) => {
   try {
@@ -10,6 +12,17 @@ export const melakukanAbsensi = async (nip, idStudi) => {
 
     const result = await DaftarHadirDosenDAO.insertOne(nip, idStudi, tglHariIni, true)
     return result
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
+
+export const updateKehadiranMahasiswa = async (idStudi, idJadwal, tanggal, isHadir) => {
+  try {
+    const jadwal = await JadwalDAO.findJadwalById(idJadwal)
+    const jadwalRow = jadwal[0]
+    const daftarHadirMhs = await DaftarHadirMahasiswaDAO.updateIsHadirMhs(idStudi, tanggal, jadwalRow.ja, jadwalRow.jb, isHadir)
+    return daftarHadirMhs
   } catch (error) {
     return Promise.reject(error)
   }
