826e49d98b333cc1e1e9dc79ee3646ca4d5873d9
hafizmfadli <lshafizmf1st@gmail.com>
2021-06-03 06:36:31 +0700

    Menambahkan endpoint update-kehadiran mhs

--- src/routes/DaftarHadirMahasiswa.js
+++ src/routes/DaftarHadirMahasiswa.js
@@ -6,5 +6,6 @@ const router = express.Router()
 router.put('/presensi', DaftarHadirMahasiswaController.presensiMhsHandler)
 router.get('/kelas-jadwal', DaftarHadirMahasiswaController.getDaftarHadirKelasJadwal)
 router.get('/nim-jadwal-tgl', DaftarHadirMahasiswaController.getDaftarHadirNimJadwalTgl)
+router.put('/update-kehadiran', DaftarHadirMahasiswaController.updateStatusKehadiran)
 
 export default router
