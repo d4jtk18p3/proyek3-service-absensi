350c90e52fd9d66fa15aa2cd5003692fcfe64b3f
hafizmfadli <lshafizmf1st@gmail.com>
2021-07-01 23:08:28 +0700

    Menambahkan route

--- src/routes/Bap.js
+++ src/routes/Bap.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get('/data-bap', BapController.getDataBAP)
 router.post('/upload', BapController.uploadBAP)
+router.get('/find-bap', BapController.findBap)
 
 export default router
