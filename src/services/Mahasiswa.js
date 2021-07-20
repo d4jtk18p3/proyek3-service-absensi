c9fb211c523fb702404a6438d5ed91210b626de4
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-07-20 09:22:16 +0700

    fix conflict in service mahasiswa

--- src/services/Mahasiswa.js
+++ src/services/Mahasiswa.js
@@ -1,12 +1,12 @@
 /*
   Catatan :  File ini berisi seluruh bussiness logic yang dapat dilakukan oleh mahasiswa
 */
-import * as JadwalDAO from "../dao/Jadwal";
-import * as KeteranganDAO from "../dao/Keterangan";
-import * as DaftarHadirMahasiswaDAO from "../dao/DaftarHadirMahasiswa";
-import schedule from "node-schedule";
-import { DateTime } from "luxon";
-import holiday from "../const/Liburan";
+import * as JadwalDAO from '../dao/Jadwal'
+import * as KeteranganDAO from '../dao/Keterangan'
+import * as DaftarHadirMahasiswaDAO from '../dao/DaftarHadirMahasiswa'
+import schedule from 'node-schedule'
+import { DateTime } from 'luxon'
+import holiday from '../const/Liburan'
 
 export const generateDaftarHadirMahasiswa = async () => {
   // Author : hafizmfadli
@@ -15,19 +15,19 @@ export const generateDaftarHadirMahasiswa = async () => {
 
   try {
     // hanya dijalankan ketika pertama kali app di run
-    await DaftarHadirMahasiswaDAO.bikinDaftarHadirSeluruhMhsHariIni();
+    await DaftarHadirMahasiswaDAO.bikinDaftarHadirSeluruhMhsHariIni()
   } catch (error) {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-  schedule.scheduleJob("0 0 * * *", async () => {
+  schedule.scheduleJob('0 0 * * *', async () => {
     // akan dijalankan once a day jam 00:00
     try {
-      await DaftarHadirMahasiswaDAO.bikinDaftarHadirSeluruhMhsHariIni();
+      await DaftarHadirMahasiswaDAO.bikinDaftarHadirSeluruhMhsHariIni()
     } catch (error) {
-      return Promise.reject(error);
+      return Promise.reject(error)
     }
-  });
-};
+  })
+}
 
 export const melakukanAbsensi = async (idStudi, idJadwal) => {
   // Author : hafizmfadli
@@ -36,30 +36,30 @@ export const melakukanAbsensi = async (idStudi, idJadwal) => {
   // return : rows updated
 
   try {
-    const jadwal = await JadwalDAO.findJadwalById(idJadwal);
+    const jadwal = await JadwalDAO.findJadwalById(idJadwal)
     // presensi dapat dilakukan ketika
     // 30 menit sebelum perkuliaham dimulai
     // sampai batas akhir waktu perkuliahan
-    const now = DateTime.now();
-    const tglHariIni = now.toISODate();
+    const now = DateTime.now()
+    const tglHariIni = now.toISODate()
     // const pembukaanPreseni = DateTime.fromISO(`${tglHariIni}T${jadwal[0].waktu_mulai}`).minus({ minutes: 30 })
     const batasAkhirPresensi = DateTime.fromISO(
       `${tglHariIni}T${jadwal[0].waktu_selesai}`
-    );
-    let result;
+    )
+    let result
     // if (now >= pembukaanPreseni) {
     // presensi sudah dibuka
     if (now <= batasAkhirPresensi) {
-      let keterlambatan = 0;
+      let keterlambatan = 0
       const toleransiKeterlambatan = DateTime.fromISO(
         `${tglHariIni}T${jadwal[0].batas_terakhir_absen}`
-      );
+      )
       if (now > toleransiKeterlambatan) {
         // terlambat melakukan presensi
         const keterlambatanInMs = now
           .diff(toleransiKeterlambatan)
-          .toObject().milliseconds;
-        keterlambatan = Math.round(keterlambatanInMs / 1000 / 60); // convert ke menit
+          .toObject().milliseconds
+        keterlambatan = Math.round(keterlambatanInMs / 1000 / 60) // convert ke menit
       }
       // update kehadiran
       result = await DaftarHadirMahasiswaDAO.updateStatusKehadiranMhs(
@@ -70,13 +70,13 @@ export const melakukanAbsensi = async (idStudi, idJadwal) => {
         jadwal[0].ja,
         jadwal[0].jb,
         null
-      );
+      )
     } else {
       // sudah melewati jam matkul
-      const error = new Error("Presensi sudah ditutup");
-      error.statusCode = 400;
-      error.cause = "Perkuliahan telah selesai";
-      throw error;
+      const error = new Error('Presensi sudah ditutup')
+      error.statusCode = 400
+      error.cause = 'Perkuliahan telah selesai'
+      throw error
     }
     // }
     // Ilangin constraint
@@ -88,42 +88,42 @@ export const melakukanAbsensi = async (idStudi, idJadwal) => {
     //   throw error
     // }
 
-    return result;
+    return result
   } catch (error) {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-};
+}
 
 const isLiburan = (tgl) => {
   // author : hafizmfadli
   // params : tgl (yyyy-mm-dd : string)
-  const date = new Date(tgl);
+  const date = new Date(tgl)
   if (date.getDay() === 0 || date.getDay() === 6) {
     // sabtu - minggu libur sayang
-    return true;
+    return true
   }
 
   const liburgaknih = holiday.filter((date) => {
-    return date.tanggal === tgl;
-  });
-  return liburgaknih.length > 0;
-};
+    return date.tanggal === tgl
+  })
+  return liburgaknih.length > 0
+}
 
 const keteranganLibur = (tgl) => {
   // author : hafizmfadli
   // params : tgl (yyyy-mm-dd : string)
   // return : keterangan libur
-  const date = new Date(tgl);
+  const date = new Date(tgl)
   if (date.getDay() === 0 || date.getDay() === 6) {
     // sabtu - minggu libur sayang
-    return "hari libur kuliah";
+    return 'hari libur kuliah'
   }
 
   const liburgaknih = holiday.filter((date) => {
-    return date.tanggal === tgl;
-  });
-  return liburgaknih[0].keterangan;
-};
+    return date.tanggal === tgl
+  })
+  return liburgaknih[0].keterangan
+}
 
 export const ajukanIzin = async (idJadwals, status, url, nim, tglIzin) => {
   // ide :
@@ -142,11 +142,11 @@ export const ajukanIzin = async (idJadwals, status, url, nim, tglIzin) => {
 
   try {
     if (isLiburan(tglIzin)) {
-      const ket = keteranganLibur(tglIzin);
-      const error = new Error("Tanggal tidak valid");
-      error.statusCode = 400;
-      error.cause = `${tglIzin} adalah hari libur, ${ket}`;
-      throw error;
+      const ket = keteranganLibur(tglIzin)
+      const error = new Error('Tanggal tidak valid')
+      error.statusCode = 400
+      error.cause = `${tglIzin} adalah hari libur, ${ket}`
+      throw error
     }
 
     const keterangan = await KeteranganDAO.insertKeterangan(
@@ -154,20 +154,20 @@ export const ajukanIzin = async (idJadwals, status, url, nim, tglIzin) => {
       status,
       url,
       -1
-    );
-    const tglIzinDate = new Date(tglIzin);
+    )
+    const tglIzinDate = new Date(tglIzin)
     const minggu = DaftarHadirMahasiswaDAO.calculateWeekOfMonth(
       tglIzinDate.getDate()
-    );
-    const bulan = tglIzinDate.getMonth() + 1;
+    )
+    const bulan = tglIzinDate.getMonth() + 1
 
     // get seluruh jadwal pada hari yang diajukan izin
     const jadwals = await JadwalDAO.getJadwalMhsHrTertentu(
       nim,
       tglIzinDate.getDay()
-    );
+    )
 
-    const results = [];
+    const results = []
     await Promise.all(
       jadwals.map(async (jadwal) => {
         if (idJadwals.includes(`${jadwal.id_jadwal}`)) {
@@ -177,9 +177,9 @@ export const ajukanIzin = async (idJadwals, status, url, nim, tglIzin) => {
             tglIzin,
             jadwal.ja,
             jadwal.jb
-          );
+          )
 
-          let result;
+          let result
           if (isPunya) {
             // kalo udah punya artinya dia izin untuk hari ini, cukup update yg sudah ada
             result = await DaftarHadirMahasiswaDAO.updateStatusKehadiranMhs(
@@ -190,7 +190,7 @@ export const ajukanIzin = async (idJadwals, status, url, nim, tglIzin) => {
               jadwal.ja,
               jadwal.jb,
               keterangan.dataValues.id_keterangan
-            );
+            )
           } else {
             // kalo belum punya artinya dia izin dihari yg akan datang, insert row baru
             result = await DaftarHadirMahasiswaDAO.insertOne(
@@ -203,16 +203,16 @@ export const ajukanIzin = async (idJadwals, status, url, nim, tglIzin) => {
               bulan,
               jadwal.ja,
               jadwal.jb
-            );
-            result = [result.dataValues];
+            )
+            result = [result.dataValues]
           }
-          results.push(result[0]);
-          return result;
+          results.push(result[0])
+          return result
         }
       })
-    );
-    return results;
+    )
+    return results
   } catch (error) {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-};
+}
